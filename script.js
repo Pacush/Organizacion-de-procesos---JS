ba7014e02e2c72f6de37af4736fc2ccc6b5d2966
José Sánchez <josealejandro.sancheznaranjo@gmail.com>
2024-11-05 12:24:34 -0600

    index.html creado y archivo script.js modulado

---
+++ script.js
@@ -0,0 +1,12 @@
+class Proceso {
+            
+    constructor(nombre, propietario, departamento, tiempoEjecucionEstimado, cantidadMateriales, localizacionOrigen, localizacionFinal) {
+        this.nombre = nombre;
+        this.propietario = propietario;
+        this.departamento = departamento;
+        this.tiempoEjecucionEstimado = tiempoEjecucionEstimado;
+        this.cantidadMateriales = cantidadMateriales;
+        this.localizacionOrigen = localizacionOrigen;
+        this.localizacionFinal = localizacionFinal;
+    }
+}
